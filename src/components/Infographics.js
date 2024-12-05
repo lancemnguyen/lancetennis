f89d5ae9bfabb1392ab1f1175098b4faeacac5ed
lance nguyen <lancesudo@gmail.com>
2024-12-05 02:03:29 -0800

    change env var

--- src/components/Infographics.js
+++ src/components/Infographics.js
@@ -25,7 +25,7 @@ const Infographics = () => {
     }, [selectedCity]);
 
     // location
-    const GOOGLE_MAPS_API = process.env.GOOGLE_MAPS_API;
+    const NEXT_PUBLIC_GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
     const mapContainerStyle = { width: '500px', height: '350px' };
 
     const locations = [
@@ -69,7 +69,7 @@ const Infographics = () => {
                         Courts
                     </h1>
                     <p className="flex items-center flex-1 justify-center">(Click on a marker for details)</p>
-                    <LoadScript googleMapsApiKey={GOOGLE_MAPS_API}>
+                    <LoadScript googleMapsApiKey={NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
                         <GoogleMap
                             mapContainerStyle={mapContainerStyle}
                             center={center}
